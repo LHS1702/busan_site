0719a36658b3788ca82344f1d41c3e07b0aa8a53
LHS1702 <dlgytjr1702@naver.com>
2023-02-06 03:59:56 +0900

    부산광역시 사이트

---
+++ busan_site/js/full_down.js
@@ -0,0 +1,49 @@
+// $(function(){
+//   const lnbLi = $('.menu>ul>li');
+//   const ul = $('.menu>ul>li>ul');
+//   const background = $('.header .for_header_inner_outside_background');
+//   const bgr_height = background.innerHeight;
+//   const headerMin = lnbLi.innerHeight();
+  // const headerMin = 0;
+  // const headerMax = ul.innerHeight(); 
+  // let state = false;
+
+  // lnbLi.mouseenter(function(){
+  //   if(!state)
+  //   {
+  //     ul.stop().animate
+  //     (
+  //       {height:headerMax},150,function(){
+  //         ul.stop().fadeIn(300);}
+  //     )
+  //     state = true;
+  //   }
+  // })
+
+  // $(lnbLi).mouseleave(function(){
+  //   ul.stop().fadeOut(300, function(){
+  //     ul.stop().animate({height:headerMin},150);
+  //   })
+  //   state =false;
+  // })
+//   lnbLi.mouseenter(function(){
+//     if(!state)
+//     { 
+//       background.stop().animate
+//       (
+//         {height:bgr_height},500,function(){
+//           ul.stop().fadeIn(300);}
+//       )
+//       state = true;
+//     }
+//   })
+
+//   lnbLi.mouseleave(function(){
+//     ul.fadeOut(300, function(){
+//       background.stop().animate({height:headerMin},150);
+//     })
+//     state =false;
+//   })
+
+//  })
+
